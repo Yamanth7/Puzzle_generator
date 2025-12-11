2d74b119d186ccce9e7e016d363f160d1ec2eea9
koti yamanth <kotiyamanth7@gmail.com>
2025-12-11 14:39:36 +0530

    Initial commit

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from src.puzzle_generator import generate_puzzle
 from src.tracker import PerformanceTracker
 from src.yatacku import adjust_difficulty
- 
 
 app = Flask(__name__)
 
@@ -13,9 +12,8 @@ def home():
 @app.route("/generate", methods=["POST"])
 def generate():
     difficulty = request.form.get("difficulty", "medium")
-
     puzzle = generate_puzzle(difficulty)
-    return f"<h2>Your Puzzle:</h2><p>{puzzle}</p>"
+    return render_template("index.html", puzzle=puzzle, difficulty=difficulty)
 
 if __name__ == "__main__":
     app.run(debug=True)
